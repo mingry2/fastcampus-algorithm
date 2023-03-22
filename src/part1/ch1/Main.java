14ebb4070d45f25b9a11ea2ba6843c8251e9c800
minkyoung kim <minry20415@naver.com>
2023-03-22 22:25:18 +0900

    :heavy_plus_sign: add: [1157] 단어 공부

--- src/part1/ch1/Main.java
+++ src/part1/ch1/Main.java
@@ -2,43 +2,51 @@ package part1.ch1;
 
 import java.util.Scanner;
 
-//[1919] 애너그램 만들기
+//[1157] 단어 공부
 public class Main {
-	//공통적인 기능을 뽑아 함수를 만듦
-	public static int[] get_alphabet_count(String str) {
+	public static int[] getAlphabetCount(String str) {
 		int[] count = new int[26];
 		for (int i = 0; i < str.length(); i++) {
-			count[str.charAt(i) - 'a']++; //-'a'를 해주면 알파벳이 나옴
-		}
-		return count;
+			count[str.charAt(i) - 'A']++; //Uppercase 함수를 사용하여 모두 대문자로 통일시켰음
+//			char ch = str.charAt(i);
+//			if ('A' <= ch && ch <= 'Z') {
+//				count[ch - 'A']++;
+//			} else if ('a' <= ch & ch <= 'z') {
+//				count[ch - 'a']++;
+//			}
+		} return count;
 	}
 
 	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String a = sc.next();
-		String b = sc.next();
-		//------------------------------------------- 카운트 배열 구현
-		int[] a_count = get_alphabet_count(a); //count 배열(알파벳 갯수 26개)
-		int[] b_count = get_alphabet_count(b);
-//		int[] a_count = new int[26]; //count 배열(알파벳 갯수 26개)
-//		int[] b_count = new int[26];
-//		for (int i = 0; i < a.length(); i++) {
-//			a_count[a.charAt(i) - 'a']++; //-'a'를 해주면 알파벳이 나옴
-//		}
-//		for (int i = 0; i < b.length(); i++) {
-//			b_count[b.charAt(i) - 'a']++;
-//		}
-
-		//------------------------------------------- 차이나는 숫자가 제거해줘야하는 원소들
-		int ans = 0;
+		String str = sc.next().toUpperCase(); //대소문자 상관없이 받으면 되기 때문에 toUpperCase() 함수 사용
+		int[] count = getAlphabetCount(str);
+		//count에 카운트된 숫자확인
 		for (int i = 0; i < 26; i++) {
-			//넘겨받는 인자의 절대값을 취하는 메서드 -> b가 a보다 크면 음수 나오지만 절대값이니 - 빼고 +=하게됨
-			ans += Math.abs(a_count[i] - b_count[i]);
-//			if (a_count[i] > b_count[i]) { //연산 시 음수가 나올 수 있으므로 이렇게 대소 비교를하는 것
-//				ans += a_count[i] - b_count[i];
-//			} else if (b_count[i] > a_count[i]) {
-//				ans += b_count[i] - a_count[i];
-//			}
-		} System.out.println(ans);
+			if (count[i] > 0) {
+				System.out.println((char)('A' + i) + ": " + count[i]);
+				/*
+				Mississipi
+				I: 4
+				M: 1
+				P: 1
+				S: 4
+				 */
+			}
+		}
+
+		//최댓값 구하기
+		int max_count = -1; //최댓값 갱신 시 문제 없는 -1로 설정
+		char max_alphabet = '?'; //문제에서 최댓값을 가진 알파벳을 구하라라고 했기 때문에
+		for (int i = 0; i < 26; i++) {
+			if (count[i] > max_count) {
+				max_count = count[i];
+				max_alphabet = (char)('A' + i);
+			} else if (count[i] == max_count) {
+				max_alphabet = '?';
+			}
+		}
+		System.out.println(max_alphabet);
 	}
+
 }
---
+++ src/part1/ch1/Main2.java
@@ -0,0 +1,44 @@
+package part1.ch1;
+
+import java.util.Scanner;
+
+//[1919] 애너그램 만들기
+public class Main2 {
+	//공통적인 기능을 뽑아 함수를 만듦
+	public static int[] get_alphabet_count(String str) {
+		int[] count = new int[26];
+		for (int i = 0; i < str.length(); i++) {
+			count[str.charAt(i) - 'a']++; //-'a'를 해주면 알파벳이 나옴
+		}
+		return count;
+	}
+
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		//------------------------------------------- 카운트 배열 구현
+		int[] a_count = get_alphabet_count(a); //count 배열(알파벳 갯수 26개)
+		int[] b_count = get_alphabet_count(b);
+//		int[] a_count = new int[26]; //count 배열(알파벳 갯수 26개)
+//		int[] b_count = new int[26];
+//		for (int i = 0; i < a.length(); i++) {
+//			a_count[a.charAt(i) - 'a']++; //-'a'를 해주면 알파벳이 나옴
+//		}
+//		for (int i = 0; i < b.length(); i++) {
+//			b_count[b.charAt(i) - 'a']++;
+//		}
+
+		//------------------------------------------- 차이나는 숫자가 제거해줘야하는 원소들
+		int ans = 0;
+		for (int i = 0; i < 26; i++) {
+			//넘겨받는 인자의 절대값을 취하는 메서드 -> b가 a보다 크면 음수 나오지만 절대값이니 - 빼고 +=하게됨
+			ans += Math.abs(a_count[i] - b_count[i]);
+//			if (a_count[i] > b_count[i]) { //연산 시 음수가 나올 수 있으므로 이렇게 대소 비교를하는 것
+//				ans += a_count[i] - b_count[i];
+//			} else if (b_count[i] > a_count[i]) {
+//				ans += b_count[i] - a_count[i];
+//			}
+		} System.out.println(ans);
+	}
+}
